8060d613753253b90143c4eb3c50a19ee131208a
codec <191799163+codec266@users.noreply.github.com>
2026-03-07 03:11:31 +0800

    feat(app): add new admin dashboard route

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import CustomerHome from "./pages/CustomerHome"
 import Profiles from "./pages/Profiles"
 import Orders from "./pages/Orders"
 import PlaceOrder from "./pages/PlaceOrder"
+import AdminDashboard from "./pages/AdminDashboard"
 
 export default function App() {
   return(
@@ -11,6 +12,7 @@ export default function App() {
       <Routes>
         <Route path ="/" element={<AuthForm />} />
         <Route path ="/home" element={(<CustomerHome />)} />
+        <Route path ="/admin" element={<AdminDashboard />} />
         <Route path ="/profile" element={(<Profiles />)} />
         <Route path ="/orders" element={(<Orders />)}/>
         <Route path ="/place-order" element={<PlaceOrder />} />
