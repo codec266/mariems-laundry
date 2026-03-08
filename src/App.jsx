28a033b8d5bb947be23cc4ef464e6ce5f0fed4b9
codec <191799163+codec266@users.noreply.github.com>
2026-03-08 22:27:05 +0800

    feat(add) more navigation routes in admin side

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import Profiles from "./pages/Profiles"
 import Orders from "./pages/Orders"
 import PlaceOrder from "./pages/PlaceOrder"
 import AdminDashboard from "./pages/AdminDashboard"
+import AdminPendingOrders from "./pages/AdminPendingOrders"
+import AdminOngoingOrders from "./pages/AdminOngoingOrders"
+import AdminOrderHistory from "./pages/AdminOrderHistory"
 
 export default function App() {
   return(
@@ -16,6 +19,9 @@ export default function App() {
         <Route path ="/profile" element={(<Profiles />)} />
         <Route path ="/orders" element={(<Orders />)}/>
         <Route path ="/place-order" element={<PlaceOrder />} />
+        <Route path="/admin-pending-orders" element={<AdminPendingOrders />} />
+        <Route path="/admin-ongoing-orders" element={<AdminOngoingOrders />} />
+        <Route path="/admin-order-history" element={<AdminOrderHistory />} />
       </Routes>
     </Router>
   )
